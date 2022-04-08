6bcfd32d8a154704db7b1ef1a4eb5fdb172df56b
Kasper Mikiewicz <IderedPL@gmail.com>
2022-04-08 21:34:28 +0200

    feat: improve twitter flyout on mobile

--- src/components/TheTwitterFlyout.vue
+++ src/components/TheTwitterFlyout.vue
@@ -1,24 +1,18 @@
 <template>
   <div
-    class="sm:col-start-2 fixed right-0 left-[240px] top-full grid justify-items-center max-h-0"
+    class="sm:col-start-2 fixed -mt-12 sm:mt-0 right-4 left-4 md:left-[256px] top-full grid justify-items-center max-h-0"
   >
-    {{ showTwitterFlyout }}
     <div
       class="absolute bottom-full bg-slate-800 p-2 -translate-y-5 rounded-full border border-slate-700 grid grid-flow-col items-center gap-x-4 transition ease-out-back duration-700"
       :class="{
-        'opacity-0 pointer-events-none translate-y-12':
-          !showTwitterFlyout || !store.enableTwitterFlyout,
+        'opacity-0 pointer-events-none translate-y-12': !showTwitterFlyout || !store.enableTwitterFlyout,
       }"
     >
-      <img
-        src="https://github.com/idered.png"
-        alt=""
-        class="w-8 rounded-full"
-      />
+      <img src="https://github.com/idered.png" alt="" class="w-8 rounded-full" />
 
       <span class="text-slate-400 font-medium text-sm">
-        Your first screenshot looks great <span class="ml-1"> 🎉</span> If you
-        like this tool, follow me on Twitter for more!
+        Your first screenshot looks great <span class="ml-1"> 🎉</span> If you like this tool, follow me on Twitter for
+        more!
       </span>
 
       <a
@@ -35,10 +29,7 @@
         title="Hide"
         @click="store.enableTwitterFlyout = false"
       >
-        <IconClose
-          width="24"
-          class="group-hover:scale-[1.20] transition-transform duration-500"
-        />
+        <IconClose width="24" class="group-hover:scale-[1.20] transition-transform duration-500" />
       </button>
     </div>
   </div>
