6730d13f334134c178b9d277f150418c2b7bbc1c
Kasper Mikiewicz <IderedPL@gmail.com>
2024-01-17 12:00:43 +0100

    blur particles

--- src/lib/particle.ts
+++ src/lib/particle.ts
@@ -5,15 +5,21 @@ export class Particle {
   speedX: number;
   speedY: number;
   opacity: number;
+  filter: string;
 
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
     this.opacity = Math.random() * 0.25;
     this.size = Math.random() * 2 + 2; // Particle diameter varies from 2px to 4px
-    // this.blur = Math.random() + Math.random();
     this.speedX = Math.random() * 1 - 0.5;
     this.speedY = Math.random() * 1 - 0.5;
+
+    if (Math.random() < 0.25) {
+      this.filter = `blur(2px)`; // Apply blur effect to 25% of particles
+    } else {
+      this.filter = "none"; // Remove blur effect for the remaining particles
+    }
   }
 
   update() {
@@ -26,7 +32,7 @@ export class Particle {
     ctx.lineWidth = 0;
     ctx.strokeStyle = `rgba(255, 255, 255, ${this.opacity})`; // Particle color is white
     ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
-    // ctx.filter = `blur(${this.blur}px)`;
+    ctx.filter = this.filter;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.closePath();
