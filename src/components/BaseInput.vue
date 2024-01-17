976ad0dedb87b37fe167bd1cfdcaedfcb9313767
Kasper Mikiewicz <IderedPL@gmail.com>
2024-01-17 11:49:12 +0100

    12px input font size

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    class="h-7 w-full rounded-md border border-slate-700 bg-slate-900 px-2 font-mono text-[13px] ring-blue-900/20 transition-colors hover:border-slate-600/40 focus:border-blue-800 focus:outline-none focus:ring-[3px]"
+    class="h-7 w-full rounded-md border border-slate-700 bg-slate-900 px-2 font-mono text-[12px] ring-blue-900/20 transition-colors hover:border-slate-600/40 focus:border-blue-800 focus:outline-none focus:ring-[3px]"
     type="text"
     autocomplete="off"
     spellcheck="false"
