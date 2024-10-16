60ddae9f10e9a87ee68b06f7e1cc5d8c33888d1e
Kasper Mikiewicz <IderedPL@gmail.com>
2024-10-16 22:47:12 +0200

    render html in iframe

--- src/components/ui/editor/HTMLPreview.vue
+++ src/components/ui/editor/HTMLPreview.vue
@@ -1,13 +1,48 @@
 <script setup lang="ts">
 import { CodeBlock } from "~/types";
+import { ref, onMounted, watch, nextTick } from "vue";
 
-defineProps<{
+const props = defineProps<{
   block: CodeBlock;
 }>();
+
+const iframeRef = ref<HTMLIFrameElement | null>(null);
+
+const updateIframeHeight = () => {
+  if (iframeRef.value) {
+    iframeRef.value.style.height = "auto";
+    iframeRef.value.style.height =
+      iframeRef.value.contentWindow?.document.body.scrollHeight + "px";
+  }
+};
+
+onMounted(() => {
+  updateIframeHeight();
+});
+
+watch(
+  () => props.block.content,
+  () => {
+    // Use nextTick to ensure the content has been updated
+    nextTick(updateIframeHeight);
+  },
+);
 </script>
 
 <template>
-  <div v-html="block.content" />
+  <iframe
+    ref="iframeRef"
+    :srcdoc="block.content"
+    sandbox="allow-scripts allow-same-origin"
+    class="html-preview"
+    @load="updateIframeHeight"
+  ></iframe>
 </template>
 
-<style></style>
+<style scoped>
+.html-preview {
+  width: 100%;
+  border: none;
+  overflow: hidden;
+}
+</style>
