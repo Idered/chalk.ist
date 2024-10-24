74263d21b32676b3764fa527f9b5add41a13b586
Daniel Roe <daniel@roe.dev>
2024-10-24 10:49:16 +0100

    feat: add simplistic error page
    will render server errors like 404s or 500s

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <main class="h-screen">
+    <div
+      class="flex h-full items-center justify-center bg-zinc-900 px-3 text-[13px] text-slate-300"
+    >
+      <i-ph:warning class="mr-2 text-yellow-500" />
+      <span class="font-bold mr-1">{{ error.statusCode }}</span> {{ error.message }}
+    </div>
+  </main>
+</template>
