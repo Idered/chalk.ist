2cc64d5a0906b7ed77c6a9c1302085dca961a4da
Kasper Mikiewicz <IderedPL@gmail.com>
2024-12-02 16:35:39 +0100

    Update index.get.ts

--- server/api/snippets/index.get.ts
+++ server/api/snippets/index.get.ts
@@ -5,7 +5,12 @@ export default eventHandler(async (event) => {
 
   const res = await Promise.all([
     db.select().from(tables.sharedSnippets),
-    new Promise((resolve) => setTimeout(() => resolve(["fail"]), 1000)),
+    new Promise((resolve) =>
+      setTimeout(() => {
+        db.$client.end({ timeout: 1 });
+        resolve(["fail"]);
+      }, 1000),
+    ),
   ]);
 
   // event.waitUntil(db.$client.end({ timeout: 1 }));
