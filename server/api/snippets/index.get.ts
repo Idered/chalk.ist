f89cfb55f23ca830420663a8b8642fe96afee62b
Kasper Mikiewicz <IderedPL@gmail.com>
2024-12-02 16:20:26 +0100

    Update index.get.ts

--- server/api/snippets/index.get.ts
+++ server/api/snippets/index.get.ts
@@ -3,6 +3,8 @@ import { sharedSnippets } from "~~/server/database/schema";
 export default eventHandler(async (event) => {
   const db = useDrizzle();
   const snippets = await db.select().from(tables.sharedSnippets);
-  await db.$client.end();
+
+  event.waitUntil(db.$client.end({ timeout: 1 }));
+
   return snippets;
 });
