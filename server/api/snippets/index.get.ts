868952a9a06a924fd63dfacf653b11ce668666cc
Kasper Mikiewicz <IderedPL@gmail.com>
2024-12-02 17:03:18 +0100

    Update index.get.ts

--- server/api/snippets/index.get.ts
+++ server/api/snippets/index.get.ts
@@ -2,17 +2,19 @@ import { sharedSnippets } from "~~/server/database/schema";
 
 export default eventHandler(async (event) => {
   const db = useDrizzle();
-
-  const res = await Promise.all([
-    db.select().from(tables.sharedSnippets),
-    new Promise((resolve) =>
-      setTimeout(() => {
-        db.$client.end({ timeout: 1 });
-        resolve(["fail"]);
-      }, 1000),
-    ),
-  ]);
-
+  try {
+    const res = await Promise.all([
+      db.select().from(tables.sharedSnippets),
+      new Promise((resolve) =>
+        setTimeout(() => {
+          db.$client.end({ timeout: 1 });
+          resolve(["fail"]);
+        }, 1000),
+      ),
+    ]);
+  } catch (err) {
+    return err;
+  }
   // event.waitUntil(db.$client.end({ timeout: 1 }));
 
   return res;
